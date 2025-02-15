815a853968ad940d21637ddfbaa1cc4a9c133a58
“Sangini <sanginijshetty@gmail.com>
2025-02-15 23:23:04 +0530

    .

--- Recursion/Recursion2.java
+++ Recursion/Recursion2.java
@@ -1,22 +1,24 @@
-// Print name 5 times
+// // Print name n times
 
 // import java.util.*;
 // class Recursion2 {
-//     static int cnt = 0;
+//     static int n; 
 //     static String name;
 //     public static void main(String [] args){
 //         Scanner sc = new Scanner(System.in);
 //         System.out.println("Enter name:");
 //         name = sc.next();
+//         System.out.println("Enter the number of times to be printed: ");
+//         n = sc.nextInt();
 //         sc.close();
-//         print();
+//         print(1, n);
 //     }
-//     static void print() {
-//         if(cnt == 5)
+//     static void print(int cnt, int n) {
+//         if(cnt > n)
 //         return;          
 //         System.out.println(name);
 //         cnt++;
-//         print();
+//         print(cnt, n);
         
 //     }
 // }
@@ -38,6 +40,9 @@
 //         print();
 //     }
 // }
+
+
+
 
 
 // //Print linearly from 1 to N
@@ -64,6 +69,9 @@
 // }
 
 
+
+
+
 // //Print from N to 1   //5 4 3 2 1 
 // import java.util.*;
 // class Recursion2 {
@@ -86,3 +94,101 @@
 //         print();
 //     }
 // }
+
+
+
+
+
+
+
+//1 to n using backtracing 
+
+// import java.util.Scanner;
+
+// public class Recursion2 {
+//     static int n;
+//     public static void main(String [] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter a number:");
+//         n = sc.nextInt();
+//         sc.close();
+//         func(n, n);
+//     }
+//     static void func(int i, int n){
+//         if(i<1)
+//         return;
+//         func(i-1,n);
+//         System.out.println(i);
+//     }
+// }
+
+
+
+
+
+
+// //N to 1 Backtracing
+
+// import java.util.Scanner;
+
+// public class Recursion2 {
+//     static int n;
+//     public static void main(String [] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter a number:");
+//         n = sc.nextInt();
+//         sc.close();
+//         func(1, n);
+//     }
+//     static void func(int i, int n){
+//         if(i>n)
+//         return;
+//         func(i+1,n);
+//         System.out.println(i);
+//     }
+// }
+
+
+//sum of 1 to n parameterized recursion
+
+// import java.util.Scanner;
+
+// public class Recursion2{
+//     static int n;
+//     public static void main(String [] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter a number:");
+//         n = sc.nextInt();
+//         sc.close();
+//         print(n, 0);
+//     }
+//     static void print(int i, int sum){
+//         if(i<1){
+//             System.out.println(sum);
+//             return;
+//         }
+//         print(i-1, sum+i);
+//     }
+// }
+
+
+
+//Sum of 1 to n functional recursion
+
+import java.util.*;
+public class Recursion2{
+    static int n;
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        n= sc.nextInt();
+        sc.close();
+        System.out.println(func(n));
+    }
+    static int func(int n) {
+        if(n==0){
+            return 0;
+        }
+        return n + func(n-1);
+    }
+}
