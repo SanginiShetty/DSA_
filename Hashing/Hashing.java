9aeff2a77d2a1558e083254cada41c6f343dd069
“Sangini <sanginijshetty@gmail.com>
2025-02-19 17:33:22 +0530

    .

--- Hashing/Hashing.java
+++ Hashing/Hashing.java
@@ -52,4 +52,44 @@
 
 
 // number hashing---> using array
-// character hashing----> 
+// character hashing----> using array and ASCII
+
+import java.util.*;
+public class Hashing {
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        String s;
+        System.out.println("Enter a string:");
+        s = sc.nextLine();
+
+        //pre compute
+        int [] hash = new int[256];
+        for(int i=0; i<s.length(); i++){
+            hash[s.charAt(i)-'a'] += 1;
+        }
+
+        int q;
+        System.out.println("Enter the number of queries:");
+        q = sc.nextInt();
+
+        while(q>0) {
+            char c;
+            System.out.println("Enter your queries character:");
+            c = sc.next().charAt(0);
+            //fetch
+            System.out.println(hash[c-'a']);
+            q--;
+        }
+        sc.close();
+    }
+}
+
+//input text
+// abcdabehf
+// 5
+// a
+// g
+// h
+// b
+// c
+
