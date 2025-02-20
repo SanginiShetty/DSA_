ed3c64728904365d8f963d2173d70cd2544fa729
“Sangini <sanginijshetty@gmail.com>
2025-02-20 23:32:13 +0530

    .

--- Hashing/Hashset.java
+++ Hashing/Hashset.java
@@ -1,5 +1,4 @@
 
-import java.util.HashSet;
 
 //HashSet in java
 //                        | Array | Sorted Array |   BST   |
@@ -8,6 +7,9 @@ import java.util.HashSet;
 //Delete/Remove - O(1)      O(n)       O(n)         O(h) O(n) 
 
 //HashSet store only unique values
+
+import java.util.HashSet;
+import java.util.Iterator;
 public class Hashset {
     public static void main(String args[])
     {
@@ -23,20 +25,29 @@ public class Hashset {
         
         //Size
         System.out.println("Sixe of set is: "+set.size());
+        System.out.println(set);
 
-        //Search - contains
-        if(set.contains(1)) {
-            System.out.println("Set contains 1");
-        }
-        if(!set.contains(8)) {
-            System.out.println("Does not contain 8");
-        }
+        // //Search - contains
+        // if(set.contains(1)) {
+        //     System.out.println("Set contains 1");
+        // }
+        // if(!set.contains(8)) {
+        //     System.out.println("Does not contain 8");
+        // }
         
         //Delete
-        set.remove(1);
-        System.out.println(set);
-        if(!set.contains(1)){
-            System.out.println("does not contain 1 - removed");
+        // set.remove(1);
+        // System.out.println(set);
+        // if(!set.contains(1)){
+        //     System.out.println("does not contain 1 - removed");
+        // }
+
+        //Iterator ----> before iterating it point to null 
+        //it.hasNext()-->true/false
+        Iterator it = set.iterator();
+
+        while(it.hasNext()) {
+            System.out.println(it.next());
         }
 
     }
