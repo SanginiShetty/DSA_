5a84d382dc74af3383c82b9fb7d9af76aaf9e22a
“Sangini <sanginijshetty@gmail.com>
2025-03-17 20:10:52 +0530

    .

--- Patterns/SolidRectangle.java
+++ Patterns/SolidRectangle.java
@@ -1,7 +1,6 @@
-
 public class SolidRectangle {
     public static void main(String args[]){
-        for(int i=1;i<=5;i++){
+        for(int i=1;i<=4;i++){
             for(int j=1;j<=5;j++) {
                 System.out.print("*");
             }
