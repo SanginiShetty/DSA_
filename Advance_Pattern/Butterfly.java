7ead1a4a7f9df28b74d80c9f00fac4a5e729abb6
“Sangini <sanginijshetty@gmail.com>
2025-03-20 22:16:38 +0530

    .

---
+++ Advance_Pattern/Butterfly.java
@@ -0,0 +1,39 @@
+public class Butterfly {
+    public static void main(String [] args) {
+        int n=4;
+        //1st half of butterfly - upper half
+        for(int i=1; i<=n; i++) {
+            //1st part of butterfly
+            for(int j = 1; j<=i; j++) {
+                System.out.print("*");
+            }
+
+            //spaces
+            for(int j= 1; j<=2*(n-i); j++) {
+                System.out.print(" ");
+            }
+
+            //2nd part of butterfly
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n; i>=1; i--){
+             //3rd part of butterfly
+             for (int j=1; j<=i; j++) {
+                System.out.print("*");
+             }
+              //spaces
+              for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+              }
+
+              //4th part of butterfly
+              for(int j=1; j<=i; j++) {
+                System.out.print("*");
+              }
+              System.out.println();
+        }
+    }
+}
