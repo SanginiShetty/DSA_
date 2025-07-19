caad7b4a853df9d07f7d31565e25455d0a1d6afe
“Sangini <sanginijshetty@gmail.com>
2025-07-19 21:18:46 +0530

    .

--- Maths concepts/PalindromeNo.java
+++ Maths concepts/PalindromeNo.java
@@ -1,5 +1,24 @@
+import java.util.*;
 public class PalindromeNo {
     public static void main(String[] args) {
-        Scannerr sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        isPalindrome(num);
+    }
+    public static void isPalindrome(int num){
+        int n = num;
+        int reverseno = 0;
+        int lastdigit;
+        while(n!=0) {
+            lastdigit = n%10;
+            reverseno = reverseno*10 + lastdigit;
+            n = n/10;
+        }
+        if(num == reverseno){
+            System.out.println(num +" is Palindrome");
+        }
+        else{
+            System.out.println(num+" is not palindrome");
+        }
     }
 }
