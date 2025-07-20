b752b430202717946c151ba0d59b4791435b140e
“Sangini <sanginijshetty@gmail.com>
2025-07-20 15:45:33 +0530

    .

--- Maths concepts/PrintAllDivisor.java
+++ Maths concepts/PrintAllDivisor.java
@@ -6,10 +6,19 @@ public class PrintAllDivisor{
         calculate(num);
     }
     public static void calculate(int num){
-        for(int i=1; i<=num; i++){
-            if(num%i == 0){
+        // for(int i=1; i<=num; i++){
+        //     if(num%i == 0){
+        //         System.out.print(i+ " ");
+        //     }
+        // }
+        for(int i = 1; i<= Math.sqrt(num); i++){
+            if(num%i==0){
                 System.out.print(i+ " ");
+                if(num/i!=i){
+                    System.out.print(num/i+" ");
+                }
             }
+
         }
     }
 }
