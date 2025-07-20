09b874628d14853071cec4681dbe37bddf3d8f33
“Sangini <sanginijshetty@gmail.com>
2025-07-20 15:34:36 +0530

    .

--- Maths concepts/PrintAllDivisor.java
+++ Maths concepts/PrintAllDivisor.java
@@ -1,3 +1,15 @@
+import java.util.*;
 public class PrintAllDivisor{
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        calculate(num);
+    }
+    public static void calculate(int num){
+        for(int i=1; i<=num; i++){
+            if(num%i == 0){
+                System.out.print(i);
+            }
+        }
+    }
 }
