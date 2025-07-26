e3a09e7a598c35f12e435d46d869eefb4c441305
“Sangini <sanginijshetty@gmail.com>
2025-07-26 23:39:45 +0530

    .

---
+++ Array/UnionOfSortedArray.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+public class UnionOfSortedArray {
+    public static void main(String[] args) {
+        int nums1[] = {1,2,3,4,5,6,7,8};
+        int nums2[] = {9,8,7,6};
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        HashSet<Integer> set= new HashSet<Integer>();
+        for(int i = 0; i<n1; i++){
+            set.add(nums1[i]);
+        }
+        for(int i=0; i<n2; i++){
+            set.add(nums2[i]);
+        }
+        int temp[] = new int[set.size()];
+        int i=0;
+        for(int iterate: set){
+            temp[i++]=iterate;
+        }
+        for(int j=0;j<set.size();j++){
+            System.out.print(temp[j]+" ");
+        }
+    }
+}
