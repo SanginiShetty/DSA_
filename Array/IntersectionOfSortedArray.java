8472cddf0716df43fe87fa6c2090b16e2beb82e6
“Sangini <sanginijshetty@gmail.com>
2025-07-28 19:34:06 +0530

    .

--- Array/IntersectionOfSortedArray.java
+++ Array/IntersectionOfSortedArray.java
@@ -7,18 +7,34 @@ public class IntersectionOfSortedArray {
         int n1 = nums1.length;
         int n2= nums2.length;
         ArrayList<Integer>ans = new ArrayList<>();
-        int visited[] = new int[n2];
-        for(int i=0; i<n1; i++){
-            for(int j=0; j<n2; j++){
-                if(nums1[i] == nums2[j] && visited[j]==0){
-                    ans.add(nums1[i]);
-                    visited[j] = 1;
-                    break;
-                }
-                if(nums2[j] > nums1[i]) break;
+        // int visited[] = new int[n2];
+        // for(int i=0; i<n1; i++){
+        //     for(int j=0; j<n2; j++){
+        //         if(nums1[i] == nums2[j] && visited[j]==0){
+        //             ans.add(nums1[i]);
+        //             visited[j] = 1;
+        //             break;
+        //         }
+        //         if(nums2[j] > nums1[i]) break;
+        //     }
+        // }
+        // System.out.println(ans);//brute force
+
+        //optimal solution
+        int i=0, j=0;
+        while(i<n1 && j<n2){
+            if(nums1[i]<nums2[j]){
+                i++;
+            }
+            else if(nums1[i]>nums2[j]){
+                j++;
+            }
+            else{
+                ans.add(nums1[i]);
+                i++;
+                j++;
             }
         }
         System.out.println(ans);
-        
     }
 }
