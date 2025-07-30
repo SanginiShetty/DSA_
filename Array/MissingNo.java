33c2b0df7fc827499bc4630fd64f393f602c1138
“Sangini <sanginijshetty@gmail.com>
2025-07-30 22:59:26 +0530

    .

--- Array/MissingNo.java
+++ Array/MissingNo.java
@@ -1,6 +1,11 @@
 public class MissingNo {
     public static void main(String args[]){
         int arr[] = {1,2,4,5,6};
-        for(int i =0;)
+        for(int i=0; i<arr.length; i++){
+            if((i+1)!=arr[i]){
+               System.out.println((i+1) + " is missing");
+               System.exit(0);
+            }
+        }
     }
 }
