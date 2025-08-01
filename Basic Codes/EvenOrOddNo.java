2b3b992cb4ae6712b3170781a161352928344479
“Sangini <sanginijshetty@gmail.com>
2025-08-01 22:53:51 +0530

    .

---
+++ Basic Codes/EvenOrOddNo.java
@@ -0,0 +1,11 @@
+public class EvenOrOddNo {
+    public static void main(String[] args) {
+        int num = 23;
+        if(num%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+}
