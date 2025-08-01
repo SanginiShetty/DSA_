0ace5743ed73c433f91da702e6dfe60e0ada4acc
“Sangini <sanginijshetty@gmail.com>
2025-08-01 23:05:41 +0530

    .

---
+++ Basic Codes/PositiveOraNegative.java
@@ -0,0 +1,14 @@
+public class PositiveOraNegative {
+    public static void main(String[] args) {
+        int num = 0;
+        if(num>0){
+            System.out.println("Positive");
+        }
+        else if(num<0){
+            System.out.println("Negative");
+        }
+        else{
+            System.out.println("Number is zero");
+        }
+    }
+}
---
+++ Basic Codes/RandomNo.java
@@ -0,0 +1,6 @@
+public class RandomNo {
+    public static void main(String[] args) {
+        int num = (int)(Math.random()*101);
+        System.out.println(num);
+    }
+}
