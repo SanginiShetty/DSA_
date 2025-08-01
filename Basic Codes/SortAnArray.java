a21a0b076ebe97dafdcc302129145d000d06ddb1
“Sangini <sanginijshetty@gmail.com>
2025-08-01 21:39:58 +0530

    .

--- Basic Codes/SortAnArray.java
+++ Basic Codes/SortAnArray.java
@@ -1,13 +1,16 @@
-
 import java.util.Arrays;
-
-
 public class SortAnArray {
 
     public static void main(String args[]) {
-        String str[] = {"bmw", "audi", "toyota", "hyundai"};
-        Arrays.sort(str);
-        for(String i: str){
+        // String str[] = {"bmw", "audi", "toyota", "hyundai"};
+        // Arrays.sort(str);
+        // for(String i: str){
+        //     System.out.println(i);
+        // }
+
+        int arr[] = {3,5,6,4,5,5,8,7,6,7};
+        Arrays.sort(arr);
+        for(int i:arr){
             System.out.println(i);
         }
     }
