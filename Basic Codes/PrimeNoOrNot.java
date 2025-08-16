0bcfac47b6a6428e8206c76391b41efebb14104d
“Sangini <sanginijshetty@gmail.com>
2025-08-16 18:17:49 +0530

    .

--- Basic Codes/PrimeNoOrNot.java
+++ Basic Codes/PrimeNoOrNot.java
@@ -1,10 +1,22 @@
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PrimeNoOrNot {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> arr = new ArrayList<>();
+        System.out.println("Enter number:");
+        int num = sc.nextInt();
+        int flag = 0;
+        for(int i=2; i<num/2; i++){
+            if(num%i == 0){
+                flag++;
+            }
+        }
+        if(flag>2){
+            System.out.println("Not a prime number");
+        }
+        else{
+            System.out.println("Prime number");
+        }
     }
 }
